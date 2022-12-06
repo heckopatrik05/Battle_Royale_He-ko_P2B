b2d1f75ee7d3e03000add405bb8867ed07f2255f
heckopatrik05 <heckopatrik05@gmail.com>
2022-12-06 12:55:01 +0100

    idk proc to nejde

---
+++ Enemy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Royale
+{
+    internal class Enemy
+    {
+        protected int _hp;
+        public int Id 
+        { 
+            get; set; 
+        }
+
+        public Enemy(int id, int hP = 10)
+        {
+            MaxHP = hP; 
+            HP = hP;
+            Id = id;
+        }
+
+        public int MaxHP 
+        { 
+            get; protected set; 
+        }
+        public int HP 
+        { 
+            get 
+            { 
+                return _hp; 
+            }
+            private set 
+            { 
+                if (value <= MaxHP && value >= 0) _hp = value; 
+            } 
+        }
+
+        public bool IsAlive 
+        { 
+            get { return (HP > 0); } 
+        }
+
+        public override string ToString()
+        {
+            return Id + ": " + HP + "/" + MaxHP + " " + IsAlive;
+        }
+
+        public int ReceiveDamage(int damage)
+        {
+            if (damage > HP)
+                HP = 0;
+            else
+                HP -= damage;
+            return damage;
+        }
+
+        public Enemy PickOpponent(Enemy[] opponents)
+        {
+            int i = Random.Shared.Next(opponents.Length);
+            return opponents[i];
+        }
+
+        public int Attack(int dmg, Enemy target)
+        {
+            Console.WriteLine(this + " hits " + target + " for " + dmg);
+            return target.ReceiveDamage(dmg);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Battle_Royale;
+
+Enemy e1 = new Enemy(1);
+Enemy e2 = new Enemy(2);
+Enemy[] combatants = new Enemy[] { e1, e2 };
+int combAlive = combatants.Length;
+int round = 0;
+while (combAlive > 1)
+{
+    Console.WriteLine("-- " + round++ + " --");
+    foreach (var c in combatants)
+    {
+        var x = c.PickOpponent(combatants);
+        c.Attack(2, x);
+    }
+    combAlive = 0;
+    foreach (var c in combatants)
+    {
+        if (c.IsAlive) combAlive++;
+        Console.WriteLine(c);
+    }
+}
+
+
